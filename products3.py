da912fbad81f8731d7b1bc33a9ae8fcddd8bc5ea
Earnest Hsiao <earnesthsiao@Earnests-MacBook-Air.local>
2022-06-16 11:15:45 +0800

    add for loop to print products

---
+++ products3.py
@@ -0,0 +1,12 @@
+products = []
+while True:
+	product_name = input('請輸入商品名稱： ')
+	if product_name == 'q':
+		break
+	product_price = input('請輸入商品價格： ')
+	products.append([product_name, product_price])
+print(products)
+print(products[0][0])
+
+for product in products:
+	print(product[0], '的價格是', p[1])
